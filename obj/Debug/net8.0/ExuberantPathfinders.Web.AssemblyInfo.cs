155c1e6aefda6ea880a4072308cc1f612eb895a4
Adammahdee <21070922@vnu.edu.vn>
2026-02-17 19:09:14 +0700

    Fix home auth actions and add missing Donations views to resolve runtime view errors

--- obj/Debug/net8.0/ExuberantPathfinders.Web.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExuberantPathfinders.Web.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExuberantPathfinders.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a952cb003b4658e55a91e41d4d9a596e80631253")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExuberantPathfinders.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExuberantPathfinders.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
